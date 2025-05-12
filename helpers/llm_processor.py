0f15efbba0101c910dec676b589b0faaad03bcd0
Nimra Mughal <nimra.mscsf19@iba-suk.edu.pk>
2025-05-12 11:11:55 +0500

    Mistral model is being used.

--- helpers/llm_processor.py
+++ helpers/llm_processor.py
@@ -97,7 +97,7 @@ def process_teacher_feedback_with_llm(teacher_df, selected_teacher, semester_nam
 
     indices_to_process = teacher_df.index
 
-    model_name = 'gemma2:2b'  # The model name to use
+    model_name = 'mistral'  # The model name to use
     
     progress_bar = st.progress(0)
     total = len(indices_to_process)
